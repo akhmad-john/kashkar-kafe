9643f2543c1a6edfc58594e8e1e8e2ebd7eae50b
Ахмаджон Шамсуддинов <shamsuddinov_a@domain.artelgroup.org>
2021-02-12 10:05:52 +0500

    Dockerfile changed9

--- kashkar_kafe/settings.py
+++ kashkar_kafe/settings.py
@@ -141,6 +141,6 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = (
-    normpath(join(BASE_DIR, 'kashkar-kafe/staticfiles/')),
+    os.path.join(BASE_DIR, 'staticfiles/'),
 )
 
